10fb41d5f4ef702007c1b2368cf0876a97c91aba
namekeptanonymous <113662431+namekeptanonymous@users.noreply.github.com>
2024-06-10 12:44:14 -0700

    Initial files for base PDF viewing for each assignment

--- app/client/PeerGrade/src/components/assign/PDFViewer.jsx
+++ app/client/PeerGrade/src/components/assign/PDFViewer.jsx
@@ -97,8 +97,8 @@ const PDFViewer = ({ url, scale }) => {
   };
 
   return (
-    <div className='flex justify-between items-center flex-col'>
-      <canvas ref={canvasRef} className="w-full mx-auto"></canvas>
+    <div className='flex justify-center items-center flex-col'>
+      <canvas ref={canvasRef} className="w-full mx-auto rounded-md"></canvas>
       <div className='w-full flex justify-between items-center flex-row p-3 max-w-sm'>
         <Button variant="outline" onClick={goToPreviousPage} disabled={pageNumber === 1}>Previous</Button>
         {numPages !== null && (

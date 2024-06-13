dc3e2339694108487cad4a43259ac4ea46cf9550
mahirr476 <rmahirr476@gmail.com>
2024-06-13 15:57:47 -0700

    added visibility icons and password regex to login/register

--- app/client/PeerGrade/src/components/login/LoginCard.jsx
+++ app/client/PeerGrade/src/components/login/LoginCard.jsx
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { user as users } from "@/lib/dbData"; // DB CALL: this is user data being pulled from the 'db'
 import { setCurrentUser } from '@/lib/redux/hooks/userSlice'; //REDUX slice
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 
 const LoginCard = ({ onSwitchToRegister }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
@@ -56,17 +58,22 @@ const LoginCard = ({ onSwitchToRegister }) => {
               className={`block w-full px-3 py-2 mt-1 text-gray-900 bg-gray-100 border ${error ? 'border-red-500' : 'border-gray-300'} rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
           </div>
-          <div>
+          <div className="relative">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
             <input
               id="password"
               name="password"
-              type="password"
+              type={passwordVisible ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
               className={`block w-full px-3 py-2 mt-1 text-gray-900 bg-gray-100 border ${error ? 'border-red-500' : 'border-gray-300'} rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
+            <div className="absolute mt-5 inset-y-0 right-0 pr-3 flex items-center">
+              <button type="button" onClick={() => setPasswordVisible(!passwordVisible)}>
+                {passwordVisible ? <EyeSlashIcon className="h-5 w-5 text-gray-500" /> : <EyeIcon className="h-5 w-5 text-gray-500" />}
+              </button>
+            </div>
           </div>
           {error && <p className="text-red-500 text-sm">{error}</p>}
           <div>

aa224e8fe927cd1bc746f0ae6865b46c604e68f1
Bhavya <bhavya25@students.cs.ubc.ca>
2024-06-13 20:55:09 -0700

    updated auth_pwd.js with Prisma ORM statements

--- app/server/src/routes/auth_pwd.js
+++ app/server/src/routes/auth_pwd.js
@@ -6,9 +6,24 @@ import session from 'express-session';
 import passport from 'passport';
 import { Strategy } from 'passport-local';
 
+import dotenv from "dotenv" 
+import { PrismaClient } from "@prisma/client"; 
+import  usersRouter from "./routes/users.js"; 
+// import { authRouter } from "./routes/auth.js";
+
+// Once dotenv.config() is called, env vars are available in process.env to all files. 
+// No need to import dotenv in other files.
+dotenv.config(); 
+
+const BACKEND_PORT = process.env.BACKEND_PORT;
+
+// Create a new Prisma client instance, share across all files using it in order to 
+// minimize the number of open connections to the database.
+const prisma = new PrismaClient();
+
 const { Pool } = pg;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 import bcrypt from "bcrypt";
 
 const app = express();
@@ -33,11 +48,11 @@ app.use(passport.session());
 
 // Create a new pool instance
 const pool = new Pool({
-  user: 'postgres',
+  user: process.env.DB_USER,
   host: 'localhost',
-  database: 'COSC 499',
-  password: '123456ab',
-  port: 5433, // default PostgreSQL port
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Test the database connection
@@ -49,6 +64,15 @@ pool.connect((err, client, done) => {
       done();
   }
 });
+
+async function checkUserByEmail(email) {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  return user;
+}
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -80,14 +104,41 @@ app.get("/logout", (req, res) => {
   });
 });
 
+
 app.post("/register", async (req, res) => {
   const { username, password, firstname, lastname, email, user_type } = req.body;
-  const sql = 'INSERT INTO users (username, pwd, firstname, lastname, email, user_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+  //const sql = 'INSERT INTO users (username, pwd, firstname, lastname, email, user_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
 
+  async function createUser(username, hash, firstname, lastname, email, user_type) {
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        pwd: hash,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        user_type: user_type,
+      },
+    });
+    console.log(newUser);
+    return newUser;
+  }
+
+  
   try {
-    const checkResult = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    const checkResult = checkUserByEmail(email)
+      .then(user => {
+        console.log(user);
+      })
+      .catch(e => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+    // const checkResult = await pool.query("SELECT * FROM users WHERE email = $1", [
+    //   email,
+    // ]);
 
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
@@ -98,9 +149,17 @@ app.post("/register", async (req, res) => {
           console.error("Error hashing password:", err);
         } else {
           console.log("Hashed Password:", hash);
-          const values = [username, hash, firstname, lastname, email, user_type];
-          const result = await pool.query(sql, values);
-          const user = result.rows[0];
+          //const values = [username, hash, firstname, lastname, email, user_type];
+
+          const user = createUser(username, hash, firstname, lastname, email, user_type)
+          .catch(e => {
+            throw e;
+          })
+          .finally(async () => {
+            await prisma.$disconnect();
+          });
+          //const result = await pool.query(sql, values);
+          //const user = result.rows[0];
           req.login(user, (err) => {
             console.log("success");
             res.redirect("/home");
@@ -122,9 +181,20 @@ app.post("/login", passport.authenticate("local", {
 passport.use(
   new Strategy(async function verify(email, password, cb) {
     try {
-      const result = await pool.query("SELECT * FROM users WHERE email = $1", [
-        email,
-      ]);
+      const result = checkUserByEmail(email)
+      .then(user => {
+        console.log(user);
+      })
+      .catch(e => {
+        throw e;
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+
+      // const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      //   email,
+      // ]);
       if (result.rows.length > 0) {
         const user = result.rows[0];
         const storedHashedPassword = user.pwd;
@@ -157,6 +227,6 @@ passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
 
-app.listen(port, () => {
+app.listen(BACKEND_PORT, () => {
   console.log(`Server running on port ${port}`);
 });

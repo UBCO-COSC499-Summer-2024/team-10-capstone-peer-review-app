28f588aa0047ff4e4a626dc2935542d6cfc8d80e
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2024-06-14 03:56:32 -0700

    added some endpoints for login + register

--- app/server/src/routes/auth.js
+++ app/server/src/routes/auth.js
@@ -1,18 +1,54 @@
-import express from "express";  
+import express from "express";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import passportLocalValidation from "../middleware/passportLocalValidation.js"
 
-const router = express.Router(); 
+const authRouter = (prisma) => { 
+    const router = express.Router();
 
-// JWT ** Hard. ? 
-// 
-//  bscypt hashing -> 
-//  and salting -> add salting numbers to .env
+    passportLocalValidation(passport, prisma);
 
-router.post("/login", async (req, res) => {
-    // TODO Handle Login
-}); 
+    router.post('/register', async (req, res) => {
+    // Check if user already exists with email
+    const existingUser = await prisma.user.findUnique({ where: { email: req.body.email } });
+    if (existingUser) {
+        return res.status(400).json({ message: 'User with that email already exists' }); 
+    }
+      // Hashing hardcoded salt to 10, TODO: move to .env
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = {
+          username: req.body.username,
+          password: hashedPassword,
+          email: req.body.email,  
+          firstname: req.body.firstname, 
+          lastname: req.body.lastname,
+          role: req.body.role
+      };
+      // Save the user to the database using Prisma
+      const result = await prisma.user.create({ data: user });
+      req.session.userId = result.userId; // Store user id in session
+      res.json(result);
+  });
 
-router.post("/register", async (req, res) => {
-    // TODO Handle Register
-}); 
+  router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(400).json({ message: info.message });
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        return res.json(user);
+      });
+    })(req, res, next);
+  });
 
-export {router as authRouter};
+return router;
+
+}
+
+export default authRouter;
---
+++ app/server/src/routes/instructors.js
@@ -0,0 +1,66 @@
+import express from "express"; 
+
+const instructorsRouter = (prisma) => { 
+    const router = express.Router();
+    
+
+    // add ensureInstructor middleware to api-endpoints 
+
+    // return a single user by id
+    // router.get("/:id",  async (req, res) => {
+    //     const { id } = req.params;
+    //     const user = await prisma.user.findUnique({
+    //         where: {
+    //             userId: id,
+    //         },
+    //     });
+    //     res.json(user);
+    // });
+
+    router.post("/create-class", async (req, res) => { 
+        // Assumes when a user logs in, JWT will include 
+        const instructorId = req.user.userId;
+
+        const { classname, description, startDate, endDate, term, classSize }  = req.body;
+
+        try {
+            const newClass = await prisma.class.create({
+                data: {
+                    instructorId,
+                    classname,
+                    description,
+                    startDate: new Date(startDate),
+                    endDate: new Date(endDate),
+                    term, 
+                    classSize
+                }
+            });
+            res.status(201).json(newClass);
+        } catch (error) {
+            res.status(500).json({ message: "Failed to create class" });
+        }
+    });
+
+    router.get("/my-classes", async (req, res) => {
+        // Get the user ID from the Passport user object
+        const instructorId = req.user.userId;
+    
+        try {
+            // Retrieve all classes for the instructor
+            const classes = await prisma.class.findMany({
+                where: {
+                    instructorId: instructorId
+                }
+            });
+    
+            res.status(200).json(classes);
+        } catch (error) {
+            res.status(500).json({ message: "Failed to retrieve classes" });
+        }
+    });
+
+    return router; 
+
+}
+
+export default instructorsRouter 

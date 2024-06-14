034ad8a66b4bb7074fbe5835558a712ee0c64365
Joshua Farwig <54459705+JoshFarwig@users.noreply.github.com>
2024-06-14 00:25:43 -0700

    hotfix to run logs late at night

--- .github/workflows/scripts/automated-team-log-dashboard.py
+++ .github/workflows/scripts/automated-team-log-dashboard.py
@@ -13,14 +13,14 @@ tz = timezone('America/Vancouver')
 
 # Get the current date and time
 now = datetime.now(tz)
-if now.weekday() == 1:  # If today is Tuesday
-    start_date = now - timedelta(days=4)  # Last Thursday
-    end_date = now
-elif now.weekday() == 3:  # If today is Thursday 
-    start_date = now - timedelta(days=2)  # Last Tuesday
-    end_date = now 
-else: # throw error if today is not Tuesday or Thursday  
-    raise ValueError('This script should only be run on Tuesdays or Thursdays') 
+# if now.weekday() == 1:  # If today is Tuesday
+#     start_date = now - timedelta(days=4)  # Last Thursday
+#     end_date = now
+# elif now.weekday() == 3:  # If today is Thursday 
+start_date = now - timedelta(days=2)  # Last Tuesday
+end_date = now 
+# else: # throw error if today is not Tuesday or Thursday  
+#     raise ValueError('This script should only be run on Tuesdays or Thursdays') 
 
 # Get cycle number
 try:

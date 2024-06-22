6947deb1bc0ee8aa91545444eea7cd1d08cdeb3e
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2024-06-22 15:21:04 -0700

    hotfixing shutdown prisma syntax

--- app/server/src/utils/shutdown.js
+++ app/server/src/utils/shutdown.js
@@ -1,4 +1,4 @@
-import prisma from "prisma/prismaClient";
+import prisma from "../../prisma/prismaClient.js";
 
 const shutdown = async (signal, server) => { 
     console.log(`Received ${signal}. Shutting down server...`);

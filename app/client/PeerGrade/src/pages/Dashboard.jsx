c62493e6e86d7061811d16fc9dc009cf5d6fdfab
mahirr476 <rmahirr476@gmail.com>
2024-06-30 13:40:47 -0700

    Dashbaord contextApi/classApi implemented

--- app/client/PeerGrade/src/pages/Dashboard.jsx
+++ app/client/PeerGrade/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import ClassCard from "@/components/class/ClassCard";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
@@ -14,259 +13,229 @@ import GroupCard from "@/components/class/GroupCard";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { useUser } from "@/contexts/contextHooks/useUser";
+import { getClassesByUserId, getAllAssignments } from "@/api/classApi";
 
 function Dashboard() {
-	const { user, userLoading } = useUser();
+  const { user, userLoading } = useUser();
 
-	const [classes, setClasses] = useState([]);
-	const [assignments, setAssignments] = useState([]);
-	const [reviews, setReviews] = useState([]);
+  const [classes, setClasses] = useState([]);
+  const [assignments, setAssignments] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const { toast } = useToast();
 
-	const { toast } = useToast();
+  useEffect(() => {
+    if (!userLoading && user) {
+      const fetchClasses = async () => {
+        try {
+          const classesData = await getClassesByUserId(user.userId);
+          setClasses(Array.isArray(classesData) ? classesData : []);
+        } catch (error) {
+          toast({
+            title: "Error",
+            description: "Failed to fetch classes",
+            variant: "destructive",
+          });
+        }
+      };
 
-	const iClassNames = iClass.map((cls) => ({
-		class_id: cls.class_id,
-		classname: cls.classname
-	})); // only for GroupCard
+      const fetchAssignments = async () => {
+        try {
+          const assignmentsData = await getAllAssignments(user.userId);
+          setAssignments(Array.isArray(assignmentsData) ? assignmentsData : []);
+        } catch (error) {
+          toast({
+            title: "Error",
+            description: "Failed to fetch assignments",
+            variant: "destructive",
+          });
+        }
+      };
 
-	console.log("User:", user);
+	  // will change once reviews api calls is figured out.
+      const fetchReviews = async () => {
+        try {
+          const response = await axios.get("/api/users/reviews");
+          setReviews(Array.isArray(response.data) ? response.data : []);
+        } catch (error) {
+          toast({
+            title: "Error",
+            description: "Failed to fetch reviews",
+            variant: "destructive",
+          });
+        }
+      };
 
-	useEffect(() => {
-		if (!userLoading && user) {
-			const fetchClasses = async () => {
-				try {
-					const response = await axios.post("/api/users/get-classes", {
-						userId: user.userId
-					});
-					setClasses(Array.isArray(response.data) ? response.data : []);
-				} catch (error) {
-					console.log(error);
-					toast({
-						title: "Error",
-						description: "Failed to fetch classes",
-						variant: "destructive"
-					});
-				}
-			};
+      fetchClasses();
+      fetchAssignments();
+      fetchReviews();
+    }
+  }, [user, userLoading, toast]);
 
-			const fetchAssignments = async () => {
-				try {
-					const response = await axios.post("/api/users/get-assignments", {
-						userId: user.userId
-					});
-					setAssignments(Array.isArray(response.data) ? response.data : []);
-				} catch (error) {
-					console.log(error);
-					toast({
-						title: "Error",
-						description: "Failed to fetch assignments",
-						variant: "destructive"
-					});
-				}
-			};
+  const assignmentData = assignments.filter(
+    (assignment) => assignment.evaluation_type !== "peer"
+  );
+  const reviewData = assignments.filter(
+    (assignment) => assignment.evaluation_type === "peer"
+  );
 
-			const fetchReviews = async () => {
-				try {
-					const response = await axios.get("/api/users/reviews");
-					setReviews(Array.isArray(response.data) ? response.data : []);
-				} catch (error) {
-					toast({
-						title: "Error",
-						description: "Failed to fetch reviews",
-						variant: "destructive"
-					});
-				}
-			};
+  const assignmentColumns = [
+    {
+      accessorKey: "title",
+      header: "Assignment Name",
+    },
+    {
+      accessorKey: "className",
+      header: "Class Name",
+    },
+    {
+      accessorKey: "dueDate",
+      header: ({ column }) => (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Due
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: ({ row }) => (
+        <Badge variant="destructive">{row.getValue("dueDate")}</Badge>
+      ),
+    },
+    {
+      accessorKey: "action",
+      header: "Actions",
+      cell: ({ row }) => (
+        <Link
+          to={row.original.link}
+          className="bg-green-100 text-blue-500 px-2 py-1 rounded-md"
+        >
+          {row.getValue("action")}
+        </Link>
+      ),
+    },
+  ];
 
-			fetchClasses();
-			fetchAssignments();
-			fetchReviews();
-		}
-		// Does toast need to be in the dependecy array?
-	}, [user, userLoading, toast]);
+  const reviewColumns = [
+    {
+      accessorKey: "title",
+      header: "Assignment Name",
+    },
+    {
+      accessorKey: "className",
+      header: "Class Name",
+    },
+    {
+      accessorKey: "dueDate",
+      header: ({ column }) => (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Review Due
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: ({ row }) => (
+        <Badge variant="destructive">{row.getValue("dueDate")}</Badge>
+      ),
+    },
+    {
+      accessorKey: "action",
+      header: "Actions",
+      cell: ({ row }) => (
+        <Link
+          to={row.original.link}
+          className="bg-green-100 text-blue-500 px-2 py-1 rounded-md"
+        >
+          {row.getValue("action")}
+        </Link>
+      ),
+    },
+  ];
 
-	console.log(classes);
+  if (userLoading) {
+    return <div>Loading...</div>;
+  }
 
-	// Loading state
-	const [loading, setLoading] = useState(true);
+  if (!user) {
+    return <p>No user</p>;
+  }
 
-	useEffect(() => {
-		// Simulate loading time for now. Replace this with actual API call timing.
-		const timer = setTimeout(() => {
-			setLoading(false);
-		}, 2000);
-
-		return () => clearTimeout(timer);
-	}, []);
-
-	if (!user) {
-		return <p>No user</p>;
-	}
-
-	const assignmentData = assignments.filter(
-		(assignment) => assignment.evaluation_type !== "peer"
-	);
-	const reviewData = assignments.filter(
-		(assignment) => assignment.evaluation_type === "peer"
-	);
-
-	const assignmentColumns = [
-		{
-			accessorKey: "title",
-			header: "Assignment Name"
-		},
-		{
-			accessorKey: "className",
-			header: "Class Name"
-		},
-		{
-			accessorKey: "dueDate",
-			header: ({ column }) => {
-				return (
-					<Button
-						variant="ghost"
-						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-					>
-						Due
-						<ArrowUpDown className="ml-2 h-4 w-4" />
-					</Button>
-				);
-			},
-			cell: ({ row }) => (
-				<Badge variant="destructive">{row.getValue("dueDate")}</Badge>
-			)
-		},
-		{
-			accessorKey: "action",
-			header: "Actions",
-			cell: ({ row }) => (
-				<Link
-					to={row.original.link}
-					className="bg-green-100 text-blue-500 px-2 py-1 rounded-md"
-				>
-					{row.getValue("action")}
-				</Link>
-			)
-		}
-	];
-
-	const reviewColumns = [
-		{
-			accessorKey: "title",
-			header: "Assignment Name"
-		},
-		{
-			accessorKey: "className",
-			header: "Class Name"
-		},
-		{
-			accessorKey: "dueDate",
-			header: ({ column }) => {
-				return (
-					<Button
-						variant="ghost"
-						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-					>
-						Review Due
-						<ArrowUpDown className="ml-2 h-4 w-4" />
-					</Button>
-				);
-			},
-			cell: ({ row }) => (
-				<Badge variant="destructive">{row.getValue("dueDate")}</Badge>
-			)
-		},
-		{
-			accessorKey: "action",
-			header: "Actions",
-			cell: ({ row }) => (
-				<Link
-					to={row.original.link}
-					className="bg-green-100 text-blue-500 px-2 py-1 rounded-md"
-				>
-					{row.getValue("action")}
-				</Link>
-			)
-		}
-	];
-
-	return (
-		<div className="w-full main-container space-y-6 ">
-			<div className="grid grid-cols-1 md:grid-cols-3 gap-6 pt-5">
-				{userLoading
-					? // Skeleton for ClassCard
-						Array.from({ length: 3 }).map((_, index) => (
-							<Skeleton key={index} className="h-48 w-full rounded-lg" />
-						))
-					: classes.map((classItem) => (
-							<ClassCard
-								key={classItem.classId}
-								classId={classItem.classId}
-								className={classItem.classname}
-								instructor={`${classItem.instructor.firstname} ${classItem.instructor.lastname}`}
-								numStudents={classItem.classSize}
-								term={classItem.term}
-							/>
-						))}
-			</div>
-			<div className="flex justify-between items-start gap-5 pt-5">
-				{/* Need to refactor group card */}
-				{/*
-				
-				<div className="flex w-1/2">
-					{userLoading ? (
-						// Skeleton for GroupCard
-						<Skeleton className="h-96 w-full rounded-lg" />
-					) : (
-						<GroupCard
-							classes={[iClass[0], iClass[1], iClass[2]]}
-							groups={Group}
-							classNames={iClassNames}
-							users={user}
-						/>
-					)}
-				</div> */}
-				<div className="flex w-3/4">
-					<Tabs defaultValue="assignments" className="flex-1">
-						{userLoading ? (
-							<Skeleton className="h-48 w-full rounded-lg" />
-						) : (
-							<TabsList className="grid w-1/3 grid-cols-2">
-								<TabsTrigger value="assignments">Assignments</TabsTrigger>
-								<TabsTrigger value="reviews">Reviews</TabsTrigger>
-							</TabsList>
-						)}
-						<TabsContent value="assignments">
-							{userLoading ? (
-								// Skeleton for DataTable
-								<Skeleton className="h-48 w-full rounded-lg" />
-							) : (
-								<DataTable
-									title="Upcoming Assignments"
-									data={assignmentData}
-									columns={assignmentColumns}
-									pageSize={4}
-								/>
-							)}
-						</TabsContent>
-						<TabsContent value="reviews">
-							{userLoading ? (
-								// Skeleton for DataTable
-								<Skeleton className="h-48 w-full rounded-lg" />
-							) : (
-								<DataTable
-									title="Upcoming Reviews"
-									data={reviewData}
-									columns={reviewColumns}
-									pageSize={4}
-								/>
-							)}
-						</TabsContent>
-					</Tabs>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full main-container space-y-6 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 pt-5">
+        {userLoading
+          ? Array.from({ length: 3 }).map((_, index) => (
+              <Skeleton key={index} className="h-48 w-full rounded-lg" />
+            ))
+          : classes.map((classItem) => (
+              <ClassCard
+                key={classItem.classId}
+                classId={classItem.classId}
+                className={classItem.classname}
+                instructor={`${classItem.instructor.firstname} ${classItem.instructor.lastname}`}
+                numStudents={classItem.classSize}
+                term={classItem.term}
+              />
+            ))}
+      </div>
+      <div className="flex justify-between items-start gap-5 pt-5">
+		{/* Need to refactor group card */}
+		{/*
+		<div className="flex w-1/2">
+			{userLoading ? (
+				// Skeleton for GroupCard
+				<Skeleton className="h-96 w-full rounded-lg" />
+			) : (
+				<GroupCard
+					classes={[iClass[0], iClass[1], iClass[2]]}
+					groups={Group}
+					classNames={iClassNames}
+					users={user}
+				/>
+			)}
+		</div> */}
+        <div className="flex w-3/4">
+          <Tabs defaultValue="assignments" className="flex-1">
+            {userLoading ? (
+              <Skeleton className="h-48 w-full rounded-lg" />
+            ) : (
+              <TabsList className="grid w-1/3 grid-cols-2">
+                <TabsTrigger value="assignments">Assignments</TabsTrigger>
+                <TabsTrigger value="reviews">Reviews</TabsTrigger>
+              </TabsList>
+            )}
+            <TabsContent value="assignments">
+              {userLoading ? (
+                <Skeleton className="h-48 w-full rounded-lg" />
+              ) : (
+                <DataTable
+                  title="Upcoming Assignments"
+                  data={assignmentData}
+                  columns={assignmentColumns}
+                  pageSize={4}
+                />
+              )}
+            </TabsContent>
+            <TabsContent value="reviews">
+              {userLoading ? (
+                <Skeleton className="h-48 w-full rounded-lg" />
+              ) : (
+                <DataTable
+                  title="Upcoming Reviews"
+                  data={reviewData}
+                  columns={reviewColumns}
+                  pageSize={4}
+                />
+              )}
+            </TabsContent>
+			
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;

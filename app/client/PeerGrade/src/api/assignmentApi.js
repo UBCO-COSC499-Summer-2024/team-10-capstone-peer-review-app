bfad35a27da0cf2fdc6c5eaad4b5e316bef1a129
mahirr476 <rmahirr476@gmail.com>
2024-07-01 19:26:37 -0400

    adding assignment API ui fetch file

---
+++ app/client/PeerGrade/src/api/assignmentApi.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+import showStatusToast from "@/utils/showToastStatus";
+
+const BASE_URL = "/api"; // Use environment variable if available
+
+export const addAssignmentToClass = async (classId, assignmentData) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/classes/${classId}/assignments`, assignmentData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return error.response.data;
+    }
+};
+
+export const updateAssignmentInClass = async (classId, assignmentId, updateData) => {
+    try {
+        const response = await axios.put(`${BASE_URL}/classes/${classId}/assignments/${assignmentId}`, updateData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return error.response.data;
+    }
+};
+
+export const removeAssignmentFromClass = async (classId, assignmentId) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/classes/${classId}/assignments/${assignmentId}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return error.response.data;
+    }
+};
+
+export const getAssignmentInClass = async (classId, assignmentId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/classes/${classId}/assignments/${assignmentId}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return error.response.data;
+    }
+};
+
+export const getAllAssignmentsByClassId = async (classId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/classes/${classId}/assignments`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return error.response.data;
+    }
+};
+
+export const addAssignmentToCategory = async (classId, categoryId, assignmentData) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/classes/${classId}/categories/${categoryId}/assignments`, assignmentData);
+      return response.data.data;
+    } catch (error) {
+      handleError(error);
+      throw error;
+    }
+  };
+
+function handleError(error) {
+    if (error.response && error.response.data) {
+        showStatusToast({
+            status: error.response.data.status,
+            message: error.response.data.message
+        });
+    } else {
+        showStatusToast({
+            status: "Error",
+            message: "An unexpected error occurred. Please try again."
+        });
+    }
+}

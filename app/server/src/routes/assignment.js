a67aed9c64f3db3e1d98416b13cde70ee5e951b1
mahirr476 <rmahirr476@gmail.com>
2024-07-02 16:54:59 -0400

    fixed routing missing ensureUser ensureInstructor

--- app/server/src/routes/assignment.js
+++ app/server/src/routes/assignment.js
@@ -23,7 +23,7 @@ router.route("/update-assignment")
   .post(ensureUser, ensureInstructor, updateAssignmentInClass);
 
   router.route("/get-assignment")
-  .post( getAssignmentInClass);
+  .post(ensureUser, ensureInstructor, getAssignmentInClass);
 
 router.route("/get-class-assignments")
   .post(ensureUser, getAllAssignmentsByClassId);

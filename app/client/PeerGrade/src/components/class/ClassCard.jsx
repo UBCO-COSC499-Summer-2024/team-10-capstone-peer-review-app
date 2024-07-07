3315588db9c3a338cbe6249ec7a951ac9bf02b26
namekeptanonymous <abdulwahab3703@gmail.com>
2024-07-06 17:38:18 -0700

    UI tweaks to the Dashboard + ClassCard

--- app/client/PeerGrade/src/components/class/ClassCard.jsx
+++ app/client/PeerGrade/src/components/class/ClassCard.jsx
@@ -18,30 +18,30 @@ const ClassCard = ({
 }) => {
 	return (
 		<Link to={`/class/${classId}`}>
-			<Card className="w-full p-1 flex items-center justify-between bg-white shadow-md rounded-lg hover:shadow-lg ">
-				<CardHeader className=" border-r-3">
+			<Card className="w-full p-1 flex items-center justify-between bg-white shadow-md rounded-lg hover:shadow-lg overflow-hidden h-full">
+				<CardHeader className="border-r-3 pr-0 border-collapse">
 					<CardTitle className="text-xl font-bold">{className}</CardTitle>
 					<CardDescription className="text-gray-500">
 						{instructor}
 					</CardDescription>
 				</CardHeader>
-				<CardContent className="flex flex-col space-y-2">
+				<CardContent className="flex flex-col space-y-2 pl-0 border-collapse">
 					<div className="flex items-center space-x-2">
 						<User className="w-4 h-4 text-gray-700" />
 						<span className="text-gray-700">{numStudents} Students</span>
 					</div>
-					<div className="flex items-center space-x-2">
+					{numAssignments && <div className="flex items-center space-x-2">
 						<FileText className="w-4 h-4 text-gray-700" />
 						<span className="text-gray-700">
 							{numAssignments} Assignments Due
 						</span>
-					</div>
-					<div className="flex items-center space-x-2">
+					</div>}
+					{numPeerReviews && <div className="flex items-center space-x-2">
 						<CheckSquare className="w-4 h-4 text-gray-700" />
 						<span className="text-gray-700">
 							{numPeerReviews} Peer Reviews Left
 						</span>
-					</div>
+					</div>}
 				</CardContent>
 			</Card>
 		</Link>

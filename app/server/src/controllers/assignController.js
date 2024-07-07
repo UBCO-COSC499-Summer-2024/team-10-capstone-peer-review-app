4d0015474f4487fa79127878aae27ca0b89b8516
mahirr476 <rmahirr476@gmail.com>
2024-07-06 21:41:34 -0700

    FILE STORAGE AND ADD ASSIGNMENT WORKS WOOOOOO

--- app/server/src/controllers/assignController.js
+++ app/server/src/controllers/assignController.js
@@ -1,19 +1,37 @@
 import assignService from "../services/assignService.js";
 import asyncErrorHandler from "../utils/asyncErrorHandler.js";
+import multer from 'multer';
 
-// Controller methods for assignment operations
-export const addAssignmentToClass = asyncErrorHandler(async (req, res) => {
-	const { classId, assignmentData } = req.body;
-	const updatedClass = await assignService.addAssignmentToClass(
-		classId,
-		assignmentData
-	);
-	return res.status(200).json({
-		status: "Success",
-		message: "Assignment successfully added to class",
-		data: updatedClass
-	});
-});
+// Configure Multer for file upload
+const upload = multer({ dest: 'uploads/' });
+
+export const addAssignmentToClass = [
+    upload.single('file'), // Handle single file upload
+    asyncErrorHandler(async (req, res) => {
+        const classId = req.body.classId;
+		const categoryId = req.body.categoryId;
+        const assignmentData = JSON.parse(req.body.assignmentData);
+        const fileUrl = req.file ? `uploads/${req.file.filename}` : null; // Construct file URL
+
+        const newAssignment = await assignService.addAssignmentToClass(classId, categoryId, {
+            ...assignmentData,
+            assignmentFilePath: fileUrl // Add file URL to assignment data
+        });
+
+        if (newAssignment) {
+            return res.status(200).json({
+                status: 'Success',
+                message: 'Assignment successfully added to class and category',
+                data: newAssignment
+            });
+        } else {
+            return res.status(500).json({
+                status: 'Error',
+                message: 'Failed to add assignment to class and category'
+            });
+        }
+    })
+];
 
 export const removeAssignmentFromClass = asyncErrorHandler(async (req, res) => {
 	const { assignmentId } = req.body;

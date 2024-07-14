b9dc35fb6ae773c05269bcaf1defb1efce8e9e21
namekeptanonymous <abdulwahab3703@gmail.com>
2024-07-13 17:51:17 -0700

    Small error page tweak (links to admin dashboard for admin)

--- app/client/PeerGrade/src/components/global/NotFound.jsx
+++ app/client/PeerGrade/src/components/global/NotFound.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import { useUser } from "@/contexts/contextHooks/useUser";
 
 const NotFound = () => {
+	const { user, userLoading } = useUser();
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
 			<h1 className="text-3xl font-bold text-gray-800">404</h1>
 			<p className="text-xl text-gray-600">Oops! Page not found</p>
-			<Link to="/dashboard" className="text-blue-500 hover:underline">
+			{user && <Link to={user.role === "ADMIN" ? "/admin" : "/dashboard"} className="text-blue-500 hover:underline">
 				Go to Dashboard
-			</Link>
+			</Link>}
 		</div>
 	);
 };

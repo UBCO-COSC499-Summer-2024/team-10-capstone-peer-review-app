5d0e0c54f17341298d9c2a155c41140e3451b273
namekeptanonymous <abdulwahab3703@gmail.com>
2024-07-18 21:07:33 -0700

    Added more students to seed

--- app/server/prisma/seed.js
+++ app/server/prisma/seed.js
@@ -24,7 +24,8 @@ async function main() {
 		return await bcrypt.hash(password, SALT_ROUNDS);
 	}
 
-	// Create users
+	// Create students
+
 	const student = await prisma.user.create({
 		data: {
 			email: "student@gmail.com",
@@ -36,7 +37,56 @@ async function main() {
 			role: "STUDENT"
 		}
 	});
+	
+	const student1 = await prisma.user.create({
+		data: {
+			email: "student1@gmail.com",
+			password: await hashedPassword("Student@123"),
+			firstname: faker.person.firstName(),
+			lastname: faker.person.lastName(),
+			isEmailVerified: true,
+			isRoleActivated: true,
+			role: "STUDENT"
+		}
+	});
+	
+	const student2 = await prisma.user.create({
+		data: {
+			email: "student2@gmail.com",
+			password: await hashedPassword("Student@123"),
+			firstname: faker.person.firstName(),
+			lastname: faker.person.lastName(),
+			isEmailVerified: true,
+			isRoleActivated: true,
+			role: "STUDENT"
+		}
+	});
+	
+	const student3 = await prisma.user.create({
+		data: {
+			email: "student3@gmail.com",
+			password: await hashedPassword("Student@123"),
+			firstname: faker.person.firstName(),
+			lastname: faker.person.lastName(),
+			isEmailVerified: true,
+			isRoleActivated: true,
+			role: "STUDENT"
+		}
+	});
+	
+	const student4 = await prisma.user.create({
+		data: {
+			email: "student4@gmail.com",
+			password: await hashedPassword("Student@123"),
+			firstname: faker.person.firstName(),
+			lastname: faker.person.lastName(),
+			isEmailVerified: true,
+			isRoleActivated: true,
+			role: "STUDENT"
+		}
+	});
 
+	// Create instructors & admin
 	const instructor = await prisma.user.create({
 		data: {
 			email: "instructor@gmail.com",
@@ -128,6 +178,36 @@ async function main() {
 	await prisma.userInClass.create({
 		data: {
 			userId: student.userId,
+			classId: class4.classId
+		}
+	});
+
+	// Enroll student1 in classes
+	await prisma.userInClass.create({
+		data: {
+			userId: student1.userId,
+			classId: class1.classId
+		}
+	});
+
+	await prisma.userInClass.create({
+		data: {
+			userId: student1.userId,
+			classId: class2.classId
+		}
+	});
+
+	// Enroll student2 in classes
+	await prisma.userInClass.create({
+		data: {
+			userId: student2.userId,
+			classId: class3.classId
+		}
+	});
+
+	await prisma.userInClass.create({
+		data: {
+			userId: student2.userId,
 			classId: class4.classId
 		}
 	});

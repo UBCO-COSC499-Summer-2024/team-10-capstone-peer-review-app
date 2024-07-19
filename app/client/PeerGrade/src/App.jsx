ded6f39e07044a4310d4971d2a697b40396adab4
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2024-07-18 19:26:41 -0700

    login center issue

--- app/client/PeerGrade/src/App.jsx
+++ app/client/PeerGrade/src/App.jsx
@@ -54,7 +54,7 @@ function MainLayout() {
 	return (
 		<div className="flex gradient-background min-h-screen">
 		{!isLoginPage && <AppNavbar />}
-		<main className={`flex-grow ${!isLoginPage ? 'ml-[200px]' : ''} flex justify-center`}>
+		<main className={`flex-grow ml-[200px] flex justify-center`}>
 		  <div className="w-full max-w-7xl px-6 py-8">
 				<Routes>
 					<Route path="/" element={<Login />} />
